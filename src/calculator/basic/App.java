d8c01bf05a5d422f1198a534187b80431b816c09
jh-01 <were1117@sm.ac.kr>
2025-04-16 13:36:58 +0900

    Feat: "[레벨1] 사칙연산 기호(➕,➖,✖️,➗)를 입력받는 코드 추가"
    계산 수행에 필요한 사칙연산 기호(➕,➖,✖️,➗)를 사용자에게 입력받고 입력이 잘 이루어졌는지 확인하는 코드 구현
    
    Closes: #1

--- src/calculator/basic/App.java
+++ src/calculator/basic/App.java
@@ -10,8 +10,13 @@ public class App {
         int firstNumber = sc.nextInt();
         System.out.print("두 번째 숫자를 입력하세요: ");
         int secondNumber = sc.nextInt();
+        System.out.print("사칙연산 기호를 입력하세요: ");
+        char operator = sc.next().charAt(0);
 
-        System.out.println("첫번째 숫자 : " + firstNumber);
-        System.out.println("두번째 숫자 : " + secondNumber);
+        System.out.println("첫 번째 숫자 : " + firstNumber);
+        System.out.println("두 번째 숫자 : " + secondNumber);
+        System.out.println("사칙연산 : " + operator);
+
+        sc.close();
     }
 }
