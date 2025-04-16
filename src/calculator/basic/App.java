43832585d84020efbb19fcc04fd64c5a806f3e89
jh-01 <were1117@sm.ac.kr>
2025-04-16 13:56:07 +0900

    Refactor: "[레벨1] 숫자 입력 방식을 nextLine 기반으로 개선"
    - 입력 유효성 검사를 강화하기 위해 문자열 기반 입력으로 전환
    - 입력 버퍼 문제 해결 및 사용자 경험 개선
    - try & catch 문 활용
    
    Closes: #1

--- src/calculator/basic/App.java
+++ src/calculator/basic/App.java
@@ -4,17 +4,31 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
+        int firstNumber;
+        int secondNumber;
+        boolean isVaild = true;
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("첫 번째 숫자를 입력하세요: ");
-        int firstNumber = sc.nextInt();
-        System.out.print("두 번째 숫자를 입력하세요: ");
-        int secondNumber = sc.nextInt();
+        while (isVaild){
+            System.out.print("첫 번째 숫자를 입력하세요: ");
+            String fistInput = sc.nextLine();
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            String secondInput = sc.nextLine();
+
+            try{
+                firstNumber = Integer.parseInt(fistInput);
+                secondNumber = Integer.parseInt(secondInput);
+                System.out.println("첫 번째 숫자 : " + firstNumber);
+                System.out.println("두 번째 숫자 : " + secondNumber);
+                isVaild = false;
+            } catch (NumberFormatException e) {
+                System.out.println("숫자를 입력해주세요!!");
+            }
+        }
+
         System.out.print("사칙연산 기호를 입력하세요: ");
         char operator = sc.next().charAt(0);
 
-        System.out.println("첫 번째 숫자 : " + firstNumber);
-        System.out.println("두 번째 숫자 : " + secondNumber);
         System.out.println("사칙연산 : " + operator);
 
         sc.close();
