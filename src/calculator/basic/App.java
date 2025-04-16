cab790941aeb4895c7c5a36a57c6edfdb9d72aa8
jh-01 <were1117@sm.ac.kr>
2025-04-16 14:44:29 +0900

    Feat: "[레벨1] “exit” 문자열을 입력하기 전까지 무한으로 계산기 기능을 반복하도록 소스 코드 작성"
    - 연산 결과 출력 후 “exit” 문자열을 입력하면 계산기 프로그램을 끝내도록 함
    
    Closes: #1

--- src/calculator/basic/App.java
+++ src/calculator/basic/App.java
@@ -8,52 +8,64 @@ public class App {
         int secondNumber = 0;
         int result = 0;
         char operator;
-        boolean isVaild = true;
+        boolean isValid = true;
         Scanner sc = new Scanner(System.in);
 
-        while (isVaild){
-            System.out.print("첫 번째 숫자를 입력하세요: ");
-            String fistInput = sc.nextLine();
-            System.out.print("두 번째 숫자를 입력하세요: ");
-            String secondInput = sc.nextLine();
+        while (true){
+            while (isValid) {
+                System.out.print("첫 번째 숫자를 입력하세요: ");
+                String fistInput = sc.nextLine();
+                System.out.print("두 번째 숫자를 입력하세요: ");
+                String secondInput = sc.nextLine();
 
-            try{
-                firstNumber = Integer.parseInt(fistInput);
-                secondNumber = Integer.parseInt(secondInput);
-                isVaild = false;
-            } catch (NumberFormatException e) {
-                System.out.println("숫자를 입력해주세요!!");
+                try {
+                    firstNumber = Integer.parseInt(fistInput);
+                    secondNumber = Integer.parseInt(secondInput);
+                    isValid = false;
+                } catch (NumberFormatException e) {
+                    System.out.println("숫자를 입력해주세요!!");
+                }
             }
-        }
 
-        System.out.print("사칙연산 기호(+, -, *, /)를 입력하세요: ");
-        String operatorInput = sc.next();
-
-        while (!(operatorInput.equals("+") || operatorInput.equals("-") || operatorInput.equals("*") || operatorInput.equals("/"))) {
-            System.out.println("⚠️ 잘못된 입력입니다. 사칙연산 기호(+, -, *, /)만 입력해주세요.");
             System.out.print("사칙연산 기호(+, -, *, /)를 입력하세요: ");
-            operatorInput = sc.next();
-        }
-        operator = operatorInput.charAt(0);
-        isVaild = true;
+            String operatorInput = sc.nextLine();
 
-        switch (operator) {
-            case '+': result = firstNumber + secondNumber;
-                break;
-            case '-': result = firstNumber - secondNumber;
-                break;
-            case '*': result = firstNumber * secondNumber;
-                break;
-            case '/': if(secondNumber != 0) result = firstNumber / secondNumber;
-            else {
-                System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
-                isVaild = false;
+            while (!(operatorInput.equals("+") || operatorInput.equals("-") || operatorInput.equals("*") || operatorInput.equals("/"))) {
+                System.out.println("⚠️ 잘못된 입력입니다. 사칙연산 기호(+, -, *, /)만 입력해주세요.");
+                System.out.print("사칙연산 기호(+, -, *, /)를 입력하세요: ");
+                operatorInput = sc.next();
             }
+            operator = operatorInput.charAt(0);
+            isValid = true;
+
+            switch (operator) {
+                case '+':
+                    result = firstNumber + secondNumber;
+                    break;
+                case '-':
+                    result = firstNumber - secondNumber;
+                    break;
+                case '*':
+                    result = firstNumber * secondNumber;
+                    break;
+                case '/':
+                    if (secondNumber != 0) result = firstNumber / secondNumber;
+                    else {
+                        System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                        isValid = false;
+                    }
+                    break;
+            }
+            if (isValid) {
+                System.out.println("결과: ");
+                System.out.printf("%d %c %d = %d\n", firstNumber, operator, secondNumber, result);
+            }
+
+            System.out.print("더 계산하시겠습니까? (exit 입력 시 종료) : ");
+            String input = sc.nextLine();
+            if (input.equals("exit")) {
                 break;
-        }
-        if(isVaild) {
-            System.out.println("결과: ");
-            System.out.printf("%d %c %d = %d", firstNumber, operator, secondNumber, result);
+            }
         }
 
         sc.close();
