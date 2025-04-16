c27e952391f67e587f5556934da125cc60c08644
jh-01 <were1117@sm.ac.kr>
2025-04-16 17:40:54 +0900

    Refactor: "[레벨2] 단일 책임 원칙 적용 + exit 입력 시 종료되는 기능 수정"
    - Calculator 클래스는 연산 기능만 수행하도록 입출력 메소드의 위치를 수정
    - exit 입력 시 정상적으로 종료되도록 수정
    - 대문자로 입력해도 종료되도록 equalsIgnoreCase() 사용
    
    Closes: #2

--- src/calculator/oop/App.java
+++ src/calculator/oop/App.java
@@ -12,17 +12,55 @@ public class App {
         Calculator calculator = new Calculator();
 
         do {
-            firstNumber = calculator.readNumber(sc, "첫 번째 숫자를 입력하세요: ");
-            secondNumber = calculator.readNumber(sc, "두 번째 숫자를 입력하세요: ");
-            operator = calculator.readOperator(sc, "사칙연산 기호(+, -, *, /)를 입력하세요: ");
+            firstNumber = readNumber(sc, "첫 번째 숫자를 입력하세요: ");
+            secondNumber = readNumber(sc, "두 번째 숫자를 입력하세요: ");
+            operator = readOperator(sc, "사칙연산 기호(+, -, *, /)를 입력하세요: ");
             try {
                 result = calculator.doCalculate(firstNumber, secondNumber, operator);
                 System.out.printf("[결과] %d %c %d = %d\n", firstNumber, operator, secondNumber, result);
+                CalculateResult calculateResult = new CalculateResult(firstNumber, secondNumber, operator, result);
+                calculator.saveCalculateResult(calculateResult);
             } catch (Exception e) {
                 System.out.println("오류 발생: " + e.getMessage());
             }
-        } while (!calculator.askContinue(sc));
+        } while (!askContinue(sc));
         sc.close();
     }
+    public static int readNumber(Scanner sc, String message){
+        int inputNumber = -1;
+        boolean isValid = true;
 
+        while (isValid) {
+            System.out.print(message);
+            String input = sc.nextLine();
+
+            try {
+                inputNumber = Integer.parseInt(input);
+                isValid = false;
+            } catch (NumberFormatException e) {
+                System.out.println("숫자를 입력해주세요!!");
+            }
+        }
+        return inputNumber;
+    }
+
+    public static char readOperator(Scanner sc, String message){
+        char operator;
+        System.out.print(message);
+        String operatorInput = sc.nextLine();
+
+        while (!(operatorInput.equals("+") || operatorInput.equals("-") || operatorInput.equals("*") || operatorInput.equals("/"))) {
+            System.out.println("⚠️ 잘못된 입력입니다. 사칙연산 기호(+, -, *, /)만 입력해주세요.");
+            System.out.print("사칙연산 기호(+, -, *, /)를 입력하세요: ");
+            operatorInput = sc.next();
+        }
+        operator = operatorInput.charAt(0);
+        return operator;
+    }
+
+    public static boolean askContinue(Scanner sc){
+        System.out.print("더 계산하시겠습니까? (exit 입력 시 종료) : ");
+        String input = sc.nextLine();
+        return input.equalsIgnoreCase("exit");
+    }
 }
